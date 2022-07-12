8d4f74834d226fbb16114fba8634139701e10a3e
mhd329 <mhd329@gmail.com>
2022-07-13 00:42:30 +0900

    codeup basic 100

--- 코드업 기초 100제/codeup_6056.py
+++ 코드업 기초 100제/codeup_6056.py
@@ -1,2 +1,5 @@
 a, b = input().split()
-print((bool(int(a)) and (not bool(int(b)))) or ((not bool(int(a))) and bool(int(b))))
+
+# print((bool(int(a)) and (not bool(int(b)))) or ((not bool(int(a))) and bool(int(b))))
+
+print(bool(int(a)) != bool(int(b)))
