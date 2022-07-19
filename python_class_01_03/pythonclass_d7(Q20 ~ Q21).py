2f60b3a0c5d531632439f0c171f0d11f8599df71
mhd329 <mhd329@gmail.com>
2022-07-19 17:25:48 +0900

    updated pythonclass d7 v. 1.0.2

--- python_class_01_03/pythonclass_d7(Q20 ~ Q21).py
+++ python_class_01_03/pythonclass_d7(Q20 ~ Q21).py
@@ -3,12 +3,11 @@
 '''
 n = int(input())
 
-n = str(n)
 res = 0
 
-for i in n:
-    i = int(i)
-    res += i
+while n > 0:
+    res += n % 10
+    n //= 10
     
 print(res)
 '''
@@ -16,7 +15,7 @@ print(res)
 # 주어진 숫자를 뒤집은 결과를 출력하시오. 
 # * 문자열이 아닌 숫자로 활용해서 풀어주세요. str() 사용 금지
 '''
-n = int(input()) # 3951
+n = int(input())
 length = 0
 res = 0
 cnt = n
