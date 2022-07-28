e4d9b551a78d98108407fe5df6a4fed5e0f505bb
mhd329 <mhd329@gmail.com>
2022-07-28 23:59:11 +0900

    boj problems

--- 1065_한수/1065.py
+++ 1065_한수/1065.py
@@ -1,12 +1,12 @@
 import sys
 sys.stdin = open("1065.txt", 'r')
 
-N = int(input())
+N = int(input()) # 210
 cnt = 0
 
 for i in range(1, N + 1):
     tmp = []
-    for j in range(1, len(str(i))): # 246
+    for j in range(1, len(str(i))):
         tmp.append(int(str(i)[j]) - int(str(i)[j-1]))
     if len(list(set(tmp))) != 2:
         cnt += 1
