f2e77a4388aa6a3aa1c86358ea2342fd3235ae13
mhd329 <mhd329@gmail.com>
2022-08-06 23:51:22 +0900

    boj problem

--- 1316_그룹단어체커/1316.py
+++ 1316_그룹단어체커/1316.py
@@ -2,15 +2,23 @@ import sys
 sys.stdin = open("1316.txt", 'r')
 
 N = int(sys.stdin.readline())
+cnt = 0
 
 for _ in range(N):
-    chk = []
+    stack = []
+    ref = 1
     group_word = sys.stdin.readline().rstrip()
     
     for char in group_word:
-        if char not in chk:
-            chk.append(char)
-        else:
-            chk = []
-            break
-        ########################
+        if char not in stack:
+            stack.append(char)
+        elif char in stack:
+            if char == stack[-1]:
+                pass
+            else:
+                ref = 0
+                break
+    if ref:
+        cnt += 1
+
+print(cnt)
