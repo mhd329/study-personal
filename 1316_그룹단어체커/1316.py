faf397a830838c114698506f4ba9911c0380112e
mhd329 <mhd329@gmail.com>
2022-08-06 23:58:29 +0900

    boj problem code refectoring

--- 1316_그룹단어체커/1316.py
+++ 1316_그룹단어체커/1316.py
@@ -2,23 +2,18 @@ import sys
 sys.stdin = open("1316.txt", 'r')
 
 N = int(sys.stdin.readline())
-cnt = 0
+no = 0
 
 for _ in range(N):
     stack = []
-    ref = 1
     group_word = sys.stdin.readline().rstrip()
     
     for char in group_word:
         if char not in stack:
             stack.append(char)
         elif char in stack:
-            if char == stack[-1]:
-                pass
-            else:
-                ref = 0
+            if char != stack[-1]:
+                no += 1 # 그룹 단어가 아님
                 break
-    if ref:
-        cnt += 1
 
-print(cnt)
+print(N - no)
