a45fba65f70812eafa5c38bdca310293f6abae93
mhd329 <mhd329@gmail.com>
2022-08-20 17:13:03 +0900

    boj problem

--- 4948_베르트랑공준/4948.py
+++ 4948_베르트랑공준/4948.py
@@ -1,21 +1,24 @@
 import sys
 sys.stdin = open("4948.txt", 'r')
 
-T = True
-
-while T:
-    p = set()
-    n = int(sys.stdin.readline())
-    sqrt_ = int((2 * n) ** 0.5) + 1
-    
-    for i in range(n + 1, (2 * n) + 1):
-        for j in range(2, sqrt_):
-            if not i % j:
-                break
-        else:
-            p.add(i)
-            
+def pn_extractor(N):
+    cnt = 0
     if not n:
-        T = False
+        return False
     else:
-        print(len(p))
+        for i in range(N + 1, m + 1):
+            for j in range(2, sqrt_):
+                if not i % j:
+                    break
+            else:
+                cnt += 1
+        return cnt
+    
+while 1:
+    n = int(sys.stdin.readline())
+    m = 2 * n
+    sqrt_ = int(m ** 0.5) + 1
+    if pn_extractor(n):
+        print(pn_extractor(n))
+    else:
+        break
