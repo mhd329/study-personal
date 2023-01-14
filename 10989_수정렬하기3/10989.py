e4b86f66b0ad450abc4f0a0f6775361990eeee84
mhd329 <mhd329@gmail.com>
2023-01-14 19:52:16 +0900

    2023 1 14 boj

--- 10989_수정렬하기3/10989.py
+++ 10989_수정렬하기3/10989.py
@@ -2,11 +2,14 @@ import sys
 sys.stdin = open("10989.txt", 'r')
 
 N = int(sys.stdin.readline())
-temp = [0] * (N + 1)
-cnt = []
-num = []
+t = [0] * 10001
 
 for _ in range(N):
     n = int(sys.stdin.readline())
-    temp[n] += 1
-print(temp)
+    t[n] += 1
+
+for i in range(1, 10001):
+    j = 0
+    while j < t[i]:
+        print(i)
+        j += 1
