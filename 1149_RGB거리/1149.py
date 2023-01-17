c2c404bb2a111d9419063d752b8587122d5c3116
mhd329 <mhd329@gmail.com>
2023-01-17 23:27:01 +0900

    2023 1 17 boj

---
+++ 1149_RGB거리/1149.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open("1149.txt", 'r')
+
+N = int(sys.stdin.readline())
+dp = [[*map(int, sys.stdin.readline().split())] for _ in range(N)]
+for i in range(1, N):
+    dp[i][0] += min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] += min(dp[i-1][0], dp[i-1][2])
+    dp[i][2] += min(dp[i-1][0], dp[i-1][1])
+print(min(dp[-1]))
