5ffcea468155b297dbd98947201b60565ea2bc8b
mhd329 <mhd329@gmail.com>
2023-01-18 20:29:44 +0900

    2023 1 18 boj

--- 17298_오큰수/17298.py
+++ 17298_오큰수/17298.py
@@ -1,14 +1,11 @@
 import sys
 sys.stdin = open("17298.txt", 'r')
-
 N = int(sys.stdin.readline())
 A = [*map(int, sys.stdin.readline().split())]
-
-while len(A) != 0:
-    a = A.pop(0)
-    for i in A:
-        if i > a:
-            print(i, end = ' ')
-            break
-    else:
-        print(-1, end = ' ')
+NGE = [-1] * N
+stack = [0]
+for i in range(1, N):
+    while stack and A[i] > A[stack[-1]]:
+        NGE[stack.pop()] = A[i]
+    stack.append(i)
+print(*NGE)
