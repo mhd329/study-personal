77f2df66991804645f789811d5a9ba027e25790b
mhd329 <mhd329@gmail.com>
2023-01-23 23:47:40 +0900

    2023 1 23 boj

--- 12865_평범한배낭/12865.py
+++ 12865_평범한배낭/12865.py
@@ -1,9 +1,12 @@
 import sys
 sys.stdin = open("12865.txt", 'r')
 N, K = map(int, sys.stdin.readline().split())
-dp = [0] * (K + 1)
-for _ in range(N):
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
     w, v = map(int, sys.stdin.readline().split())
-    for i in range(w, K + 1):
-        dp[i] = max(dp[i - w] + v, dp[i])
-print(max(dp))
+    for j in range(1, K + 1):
+        if j >= w:
+            dp[i][j] = max(dp[i - 1][j - w] + v, dp[i - 1][j])
+        else:
+            dp[i][j] = dp[i - 1][j]
+print(dp[N][K])
