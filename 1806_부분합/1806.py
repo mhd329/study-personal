ef4250f8a251e22d4accedb1de3b536c8fdb6ce9
mhd329 <mhd329@gmail.com>
2023-02-02 23:56:39 +0900

    2023 2 2 boj

--- 1806_부분합/1806.py
+++ 1806_부분합/1806.py
@@ -1,28 +1,28 @@
 import sys
 sys.stdin = open("1806.txt", 'r')
 N, S = map(int, sys.stdin.readline().split())
-n = [*map(int, sys.stdin.readline().split())]
-def interval(N:int, S:int, n:list) -> int:
-    m = N # 최소 길이 초기화
-    l = 0 # 지금 길이
-    end = 0
-    total = 0
-    for start in range(N):
-        while total < S and end < N:
-            total += n[end]
-            end += 1
-            l += 1
-        if total == S:
-            if l < m:
-                m = l
-            l = 2
+n = [0] + [*map(int, sys.stdin.readline().split())]
+total = [0] * (N + 1)
+for idx in range(1, N + 1):
+    total[idx] = total[idx - 1] + n[idx]
+def interval(N, S, n):
+    flag = False
+    j = 1
+    m = N
+    for i in range(N + 1):
+        while j < N + 1 and total[j] - total[i] < S:
+            j += 1
+        if j < N + 1 and total[j] - total[i] >= S:
+            flag = True
+            if j - i < m:
+                m = j - i
             if m == 1:
                 return print(m)
-        total -= n[start]
-        l -= 1
+    if flag:
+        return print(m)
     else:
-        if total == S:
-            return print(m)
-        else:
-            return print(0)
+        return print(0)
 interval(N, S, n)
+
+# 문제를 잘 읽자!!!
+# 합이 S 인 것이 아니라 합이 S 이상이 되는 모든 값...
