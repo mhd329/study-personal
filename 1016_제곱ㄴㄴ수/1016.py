2bf5877d79e4d09d1d85c1a56658316b9018c7af
mhd329 <mhd329@gmail.com>
2023-02-04 22:04:08 +0900

    2023 2 4 boj

--- 1016_제곱ㄴㄴ수/1016.py
+++ 1016_제곱ㄴㄴ수/1016.py
@@ -1,3 +1,5 @@
+# 1차 시도
+# 시간 초과
 '''
 N, M = map(int, input().split())
 arr = []
@@ -14,6 +16,9 @@ for j in range(N, M + 1):
         cnt += 1
 print(cnt)
 '''
+
+# 2차 시도
+# 메모리 초과
 '''
 N, M = map(int, input().split())
 p = [0, 1] + [_ for _ in range(2, M + 1)]
@@ -25,13 +30,50 @@ for i in range(2, M + 1):
             cnt -= 1
 print(cnt)
 '''
+
+# 3차 시도
+# 시간 초과
+'''
 N, M = map(int, input().split())
-p = [0 for _ in range(N, M + 1)]
-cnt = M
+cnt = 0
 for i in range(N, M + 1):
-    for j in range(2, (i**0.5) + 1):
-        if p[j]:
-            p[j] = False
-            cnt -= 1
+    if i > 3:
+        for j in range(2, int(i**0.5) + 1):
+            if not i % j ** 2:
+                break
+        else:
+            cnt += 1
+    else:
+        cnt += 1
 print(cnt)
-# 뭐가 문제인거지
+'''
+
+# 4차 시도
+# 시간, 메모리를 모두 해결했다고 생각했지만 정작 답이 틀렸다.
+'''
+N, M = map(int, input().split())
+sieve = [_ for _ in range(N, M + 1)]
+cnt = 0
+total = M - N + 1
+for i in range(N, M + 1):
+    root_i = i ** 0.5
+    if int(root_i) > 1 and not i % root_i:
+        if sieve[i - N]:
+            for j in range(i - N, M - N + 1, i):
+                if sieve[j]:
+                    sieve[j] = 0
+                    cnt += 1
+print(total - cnt)
+'''
+
+# 5차 시도
+N, M = map(int, input().split())
+sieve = [_ for _ in range(N, M + 1)]
+cnt = 0
+total = M - N + 1
+for i in range(2, int((M + 1) ** 0.5)):
+    ii = i * i
+    for j in range(N, M + 1, ii):
+        if not j % ii and sieve[j - N]:
+            sieve[j - N] = 0
+            cnt += 1
