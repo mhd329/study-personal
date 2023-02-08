d08bef089485cdc47c08a9a131ab90be29d7d38f
mhd329 <mhd329@gmail.com>
2023-02-08 18:18:17 +0900

    2023 2 8 boj

--- 2133_타일채우기/2133.py
+++ 2133_타일채우기/2133.py
@@ -1,9 +1,8 @@
 N = int(input())
-
 dp = [1, 0, 3] + [0 for _ in range(N - 2)]
-
-for i in range(3, N + 1):
-    if not i % 2: # 홀수
-        dp[i] = dp[i - 2] ** 2
-
-print(dp)
+for i in range(4, N + 1):
+    if not i % 2:
+        dp[i] = dp[i - 2] * dp[2]
+        for j in range(i - 4, -1, -2):
+            dp[i] = dp[i] + (dp[j] * 2)
+print(dp[N])
