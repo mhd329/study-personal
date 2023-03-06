f6c50b77ef98a65ba61cb9da08fe9e89b7fc7592
mhd329 <mhd329@gmail.com>
2023-03-06 23:45:04 +0900

    2023 3 6 boj

--- 14502_연구소/14502.py
+++ 14502_연구소/14502.py
@@ -9,9 +9,10 @@ visited = [[0] * M for _ in range(N)]
 def bfs(m, n):
     queue = deque()
     queue.append(m, n)
-for i in range(N):
-    for j in range(M):
-        if building[i][j] == 0:
-            building[i][j] = 1
-            visited[i][j] = 1
-            continue
+def search(n, m):
+    for i in range(n):
+        for j in range(m):
+            if building[i][j] == 0 and visited[i][j] == 0:
+                building[i][j] = 1
+                visited[i][j] = 1
+                search(n, m)
