a3e43bc26f2150fb7c5cd937f606f5930cf279c0
mhd329 <mhd329@gmail.com>
2023-03-13 23:42:07 +0900

    2023 3 13 boj

--- 14502_연구소/14502.py
+++ 14502_연구소/14502.py
@@ -1,18 +1,48 @@
+# python 시간초과, pypy 정답
 import sys
+from copy import deepcopy
 from collections import deque
 sys.stdin = open("14502.txt", 'r')
 N, M = map(int, sys.stdin.readline().split())
-building = []
+labo = []
 for _ in range(N):
-    building.append([*map(int, sys.stdin.readline().split())])
-visited = [[0] * M for _ in range(N)]
-def bfs(m, n):
+    labo.append([*map(int, sys.stdin.readline().split())])
+ans = 0
+def bfs():
+    global ans
     queue = deque()
-    queue.append(m, n)
-def search(n, m):
-    for i in range(n):
-        for j in range(m):
-            if building[i][j] == 0 and visited[i][j] == 0:
-                building[i][j] = 1
-                visited[i][j] = 1
-                search(n, m)
+    temp = deepcopy(labo)
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    for i in range(N):
+        for j in range(M):
+            if temp[i][j] == 2:
+                queue.append((i, j))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if -1 < nx < N and -1 < ny < M:
+                if temp[nx][ny] == 0:
+                    temp[nx][ny] = 2
+                    queue.append((nx, ny))
+    res = 0
+    for i in range(N):
+        for j in range(M):
+            if temp[i][j] == 0:
+                res += 1
+    ans = max(res, ans)
+cnt = 0
+def wall(cnt):
+    if cnt == 3:
+        bfs()
+        return
+    for i in range(N):
+        for j in range(M):
+            if labo[i][j] == 0 :
+                labo[i][j] = 1
+                wall(cnt + 1)
+                labo[i][j] = 0
+wall(cnt)
+print(ans)
