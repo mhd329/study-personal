571389d9476dcb9dbb681a52e510440213d7c8c0
mhd329 <mhd329@gmail.com>
2023-05-20 23:45:02 +0900

    2023 5 20 react study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ConfirmButton from './components/ConfirmButton';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/button" element={<ConfirmButton />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
