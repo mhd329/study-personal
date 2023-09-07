82ae505a3366d0e4328b1037ac570defd5039799
mhd329 <mhd329@gmail.com>
2023-09-07 23:56:43 +0900

    2023 09 07 python til

---
+++ python_intermediate/01_클래스와 메소드/01_classNmethod.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, company, details):
+        self._company = company
+        self._details = details
+
+    # 매직 메서드
+    # def __str__(self):
+    #     return "str: {} - {}".format(self._company, self._details)
+
+    def __repr__(self):
+        return "repr: {} - {}".format(self._company, self._details)
+
+
+some_car = Car("K5", {"color": "white", "hp": 160, "price": 3000})
+
+print(some_car)
